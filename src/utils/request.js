614fb1dc605f335c99486f064a35c33a4261ba76
doublehappine <hh1819050591@163.com>
2021-08-09 00:09:00 +0800

    用户登录-创建页面组件并配置路由

---
+++ src/utils/request.js
@@ -0,0 +1,15 @@
+/**
+ * 基于 axios 封装的请求模块
+ */
+import axios from 'axios'
+
+// 创建一个 axios 实例，说白了就是复制了一个 axios
+// 我们通过这个实例去发请求，把需要的配置配置给这个实例来
+const request = axios.create({
+  baseURL: 'http://ttapi.research.itcast.cn/' // 请求的基础路径
+})
+
+// 导出请求方法
+export default request
+
+// 谁要使用谁
